17cfbf5ed154e654ba66fd224c506be8690826bd
gandhi0101 <117114451+gandhi0101@users.noreply.github.com>
2023-11-17 13:17:58 -0600

    metodo no estatico solucionado

--- JAX-WS/JAX-WX/SOAPPublisher.java
+++ JAX-WS/JAX-WX/SOAPPublisher.java
@@ -4,7 +4,12 @@ import javax.xml.ws.Endpoint;
 
 public class SOAPPublisher{
 
-    public static void main(String[] args){
-        Endpoint.publish("http://localhost:8888/ws/person?wsdl");
+    public void publishWebService() {
+        Endpoint.publish("http://localhost:8888/ws/person?wsdl", new YourWebServiceImplementation());
+    }
+
+    public static void main(String[] args) {
+        SOAPPublisher publisher = new SOAPPublisher();
+        publisher.publishWebService();
     }
 }

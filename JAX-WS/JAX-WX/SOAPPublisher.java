70aaae31a408e6d201f7cf9c41851c6c6928376d
gandhi0101 <117114451+gandhi0101@users.noreply.github.com>
2023-11-17 13:21:01 -0600

    un detalle en en los parametros

--- JAX-WS/JAX-WX/SOAPPublisher.java
+++ JAX-WS/JAX-WX/SOAPPublisher.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 public class SOAPPublisher{
 
     public void publishWebService() {
-        Endpoint.publish("http://localhost:8888/ws/person?wsdl", new YourWebServiceImplementation());
+        Endpoint.publish("http://localhost:8888/ws/person?wsdl");
     }
 
     public static void main(String[] args) {
